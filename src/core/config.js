63b826cf6f7105f3096c2c1129b8022dcdecd401
Lam Nguyen <46268908+LamNguyen25@users.noreply.github.com>
2020-07-16 21:08:31 -0700

    Update config.js

--- src/core/config.js
+++ src/core/config.js
@@ -1,9 +1,10 @@
+// Replace with your own firebase config!
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyDRvboDpVWNMGT5XgmcsGB6RMsTcCJAZdM",
-    authDomain: "dictionary-2e681.firebaseapp.com",
-    databaseURL: "https://dictionary-2e681.firebaseio.com",
-    projectId: "dictionary-2e681",
-    storageBucket: "dictionary-2e681.appspot.com",
+    apiKey: "*******************************",
+    authDomain: "**************.firebaseapp.com",
+    databaseURL: "https://**********-*****.firebaseio.com",
+    projectId: "********-*****",
+    storageBucket: "**********-*****.appspot.com",
     messagingSenderId: "123962698833",
     appId: "1:123962698833:web:e41c3cf333fe11d5fa5c70",
     measurementId: "G-XRHYMVN9KP"
